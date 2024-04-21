3c1f3a9b8db44ca5e55e4224c9ea6e0e38195ed3
yangrunhan <yangrunhan@corp.netease.com>
2024-04-21 21:40:49 +0800

    add q_teacher network

--- algos/model.py
+++ algos/model.py
@@ -127,6 +127,7 @@ class LSTMBase(NNBase):
     def init_states(self, device, num_trajs=1):
         return (torch.zeros(self.core.num_layers, num_trajs, self.core.hidden_size).to(device),
                 torch.zeros(self.core.num_layers, num_trajs, self.core.hidden_size).to(device))
+    print(111)
 
     def forward(self, obs, masks, states):
         input_dim = len(obs.size())
